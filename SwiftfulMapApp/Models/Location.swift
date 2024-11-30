1ecb5e2b2639f24639aa1156abcd7daf5d2cd0fc
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-11-30 15:57:35 +0100

    Project setup with Assets.

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by user268168 on 11/30/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+}
