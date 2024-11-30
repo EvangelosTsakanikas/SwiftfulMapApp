2506c611aca9398dec538729e8c6006b7e44306b
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-12-01 00:56:37 +0100

    Created the View Model.

---
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  LocationsViewModel.swift
+//  SwiftfulMapApp
+//
+//  Created by user268168 on 12/1/24.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    
+    // All loaded locations
+    @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    // mapRegion in the bvideo
+    @Published var cameraPosition: MapCameraPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.8902,longitude: 12.4922),
+            span: MKCoordinateSpan(latitudeDelta: 0.1,longitudeDelta: 0.1)
+        )
+    )
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1,longitudeDelta: 0.1)
+    
+    
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            cameraPosition = MapCameraPosition.region(
+                MKCoordinateRegion(center: location.coordinates,span: mapSpan)
+            )
+        }
+    }
+}
