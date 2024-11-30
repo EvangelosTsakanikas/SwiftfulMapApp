a9c88186f094b99e75312fad1b0955c8db67381f
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-11-30 15:39:57 +0100

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by user268168 on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
