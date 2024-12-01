3583ce949fac72975ff07a3e278e065f3f174fd5
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-12-01 16:16:18 +0100

    Created LocationsViewModel and LocationMapAnnotationView.

---
+++ SwiftfulMapApp/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,44 @@
+//
+//  LocationMapAnnotationView.swift
+//  SwiftfulMapApp
+//
+//  Created by user268168 on 12/1/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    
+    let accentColor: Color = .accentColor
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding(6)
+                .background(accentColor)
+                .clipShape(.rect(cornerRadius: 36))
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        LocationMapAnnotationView()
+    }
+}
--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -28,16 +28,36 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(position: $vm.cameraPosition)
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
                 header
                     .padding()
-                
                 Spacer()
+                locationsPreviewStack
             }
-            
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(
+            position: $vm.cameraPosition
+        ) {
+            ForEach(vm.locations) { location in
+                
+                Annotation(
+                    location.name,
+                    coordinate: location.coordinates
+                ) {
+                    LocationMapAnnotationView()
+                        .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                        .shadow(radius: 10)
+                        .onTapGesture {
+                            vm.showNextLocation(location: location)
+                        }
+                }
+            }
         }
     }
 }
@@ -77,5 +97,21 @@ extension LocationsView {
         .background(.thickMaterial)
         .clipShape(.rect(cornerRadius: 10))
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading))
+                        )
+                }
+            }
+        }
     }
 }
